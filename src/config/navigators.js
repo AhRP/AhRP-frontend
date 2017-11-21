9230e8437719b54d69e486b683a58a0b2a3901eb
Cherie Woo <cheriew@ensenta.com>
2017-11-20 17:45:09 -0800

    Navigation working, added test directory for all jest tests, getting started with flow

--- src/config/navigators.js
+++ src/config/navigators.js
@@ -2,6 +2,7 @@ import { StackNavigator } from 'react-navigation';
 import Home from 'src/screens/Home';
 import Login from 'src/screens/Login';
 
+//Stack Navigator includes backbutton in header to nevigate back to prvious screen on stack.
 const RootNavigator = StackNavigator({
   Login: {
     screen: Login,
